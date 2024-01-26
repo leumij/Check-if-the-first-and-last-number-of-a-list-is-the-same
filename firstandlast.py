d4788bb9a54a1e0c5147b884db3065a4eb4cc4d0
leumij <cjimuel74@gmail.com>
2024-01-26 14:18:30 +0800

    pseudocode

---
+++ firstandlast.py
@@ -0,0 +1,6 @@
+# Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
+
+# Pseudocode
+# Function to create a list
+# Function to compare the first and the last element of a list
+# Insert number of elements and the elements of the list
