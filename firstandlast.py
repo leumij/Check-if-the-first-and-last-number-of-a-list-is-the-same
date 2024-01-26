080bb48a6c7bd0567e84c847d05e7e283fa6a1cc
leumij <cjimuel74@gmail.com>
2024-01-26 14:23:26 +0800

    function

--- firstandlast.py
+++ firstandlast.py
@@ -2,5 +2,20 @@
 
 # Pseudocode
 # Function to create a list
-# Function to compare the first and the last element of a list
+def first_last_same(numberList):
+    print("Given list:", numberList)
+    
+    first_num = numberList[0]
+    last_num = numberList[-1]
+    
+    if first_num == last_num:
+        return True
+    else:
+        return False
+
 # Insert number of elements and the elements of the list
+numbers_x = [10, 20, 30, 40, 10]
+print("result is", first_last_same(numbers_x))
+
+numbers_y = [75, 65, 35, 75, 30]
+print("result is", first_last_same(numbers_y))
